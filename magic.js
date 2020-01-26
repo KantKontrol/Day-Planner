6d07c67dcb2ea05ee6ccb5cacbf1ae2ab2969581
KantKontrol <nick.derissio.tech@gmail.com>
2020-01-25 20:05:04 -0500

    made times array into objects containing both display time and its 24 hour counter part, making it easy to compare

--- magic.js
+++ magic.js
@@ -4,11 +4,20 @@
 $("#currentDay").html(moment().format("MMM Do YYYY"));
 
 loadTimeBlocks();
-setTimeBlockState();
 
 function loadTimeBlocks(){
 
-    let times = ["9am", "10am","11am","12pm","1pm","2pm","3pm","4pm"];
+    let times = [
+        {d: "9am", t: "09"}, 
+        {d: "10am", t: "10"}, 
+        {d: "11am", t: "11"}, 
+        {d: "12pm", t: "12"}, 
+        {d: "1pm", t: "13"},
+        {d: "2pm", t: "14"},
+        {d: "3pm", t: "15"},
+        {d: "4pm", t: "16"},
+        {d: "5pm", t: "17"}
+    ];
 
     for(let i = 0;i < times.length;i++){
 
@@ -19,7 +28,7 @@ function loadTimeBlocks(){
         $("#timeblocks").append(newRow);
 
         newRow.append($("<div>").attr("class", "col-sm-1").append(createTimeBox(i, times[i])));
-        newRow.append($("<div>").attr("class", "col-sm-10").append(createTimeBlock(i)));
+        newRow.append($("<div>").attr("class", "col-sm-10").append(createTimeBlock(i, times[i])));
         newRow.append($("<div>").attr("class", "col-sm-1").append(createSaveButton(i)));
     }
     
@@ -30,19 +39,23 @@ function createTimeBox(rowId, time){
 
     newTimeBox.attr("class", "row hour");
 
-    newTimeBox.text(time);
+    newTimeBox.text(time.d);
 
    return newTimeBox;
 }
 
-function createTimeBlock(rowId){
+function createTimeBlock(rowId, time){
 
     let newTimeBlock = $("<div>");
-    newTimeBlock.attr("time", time.substring(0,1)); //stores hour in attribute
-    newTimeBlock.attr("class", "time-block row past");
+    newTimeBlock.attr("time", time.t); //stores hour in attribute
+    newTimeBlock.attr("class", "time-block row");
+
+    setTimeBlockState(newTimeBlock);
 
     let newTextArea = $("<textarea>");
     newTextArea.attr("class", "textarea");
+
+    
 
     newTimeBlock.append(newTextArea);
 
@@ -57,20 +70,25 @@ function createSaveButton(rowId){
     return newSaveButton;
 }
 
-function setTimeBlockState(){
-    console.log(moment().format("h"));
-
-    let tBlock = $(".time-block");
+function setTimeBlockState(tBlock){
     console.log(tBlock);
 
-    for(var i = 9;i < 8;i++){
+        let t = $(tBlock).attr("time");
 
-        //let t = $("#tb"+i).attr("time");
+        let currentTime = moment().format("HH");
+
+        if(t == currentTime){//present
+
+            $(tBlock).attr("class", "time-block row present");
+
+        }else if(t < currentTime){ //past
+
+            $(tBlock).attr("class", "time-block row past");
+
+        }else if(t > currentTime){ //future
+
+            $(tBlock).attr("class", "time-block row future");
         
-      // console.log(t);
-
-    }
-    
-
-    
+        }
+       console.log(t);
 }
